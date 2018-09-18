5b1537bb819d8aca2f96dc77fc897fe0f62731aa
Oscar F <oscarffu@gmail.com>
2018-09-18 15:49:13 -0500

    Funcionalidad eliminar

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :rooms, except: [:show]
   get '/rooms/:id/edit', to: 'rooms#edit'
   patch '/rooms/:id', to: 'rooms#update'
+  delete '/rooms/:id', to: 'rooms#destroy'
 end
